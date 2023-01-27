9581b0204f49858dc7613d71986c48a403299740
Mayank-MP05 <mayank5pande@gmail.com>
2023-01-27 16:18:31 +0530

    Timeout implemented at basic level

--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ const app = express()
 const PORT = 5000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Boiii, Welcome to Flights World!')
+})
+
+app.post('/get-flights', (req, res) => {
+    setTimeout(() => {
+        res.send('Hello Boiii, Welcome to Flights World!')
+    }, 5000)
 })
 
 app.listen(PORT, () => {
