6f72aca0d74f46d0d25fc9fa49f3012623a88367
Mayank-MP05 <mayank5pande@gmail.com>
2023-01-27 16:59:14 +0530

    sleep and timout utils added

---
+++ server/utils/index.js
@@ -0,0 +1,12 @@
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function sleep(fn, ...args) {
+    await timeout(3000);
+    return fn(...args);
+}
+
+module.exports = {
+    sleep,
+    timeout
+}
